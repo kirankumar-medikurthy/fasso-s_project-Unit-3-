cdfef0e0cb96ef0b93bd99348f800799222ad4af
kirankumar-medikurthy <kiran1999m1234@gmail.com>
2021-10-03 11:29:10 +0530

    Addtocart and orders pages

--- models/address.model.js
+++ models/address.model.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const addressSchema = new mongoose.Schema({
     flatNo: { type: String, require: true },
     landmark: { type: String, require: true },
@@ -8,15 +7,4 @@ const addressSchema = new mongoose.Schema({
 })
 
 const Address = mongoose.model("address", addressSchema);
-=======
-const addressSchema = new mongoose.Schema({
-    flatNo: { type: String, require: true },
-    landmark: { type: String, require: true },
-}, {
-    versionKey: false,
-    timestamps: true
-})
-
-const Address = mongoose.model("address", addressSchema);
->>>>>>> e21eb5e071fad4b68271a39d86a38b79b3058aed
 module.exports = Address;
